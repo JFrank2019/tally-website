04f68b547447a804a13df13c93e2d7b0f55540ad
JFrank2019 <jfrank2019@163.com>
2020-10-10 23:21:01 +0800

    deploy

---
+++ precache-manifest.cd9c2ca978b99c9732b814a33a054ba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9061cc2ef0ed483180",
+    "url": "/tally-website/css/app.edc85bcd.css"
+  },
+  {
+    "revision": "2d5a35de25516292a5990706a10a051a",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "dd9061cc2ef0ed483180",
+    "url": "/tally-website/js/app.7e562bf5.js"
+  },
+  {
+    "revision": "d98a60fc1fe0b9abe175",
+    "url": "/tally-website/js/chunk-vendors.0302bb94.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "2954ead41814fe1219c14674ba795c5a",
+    "url": "/tally-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-website/robots.txt"
+  }
+]);
